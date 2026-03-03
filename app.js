b02bf54738df33ee20e112b577c01e4ca6e44981
rockyrambo777 <sbutcher.shaun@gmail.com>
2026-03-03 07:35:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ export const SUPABASE_KEY = "sb_publishable_8NqpLKwhNvH41TjZdHygYg_iB6u1PjB";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
+
+
 export async function requireSessionOrRedirect() {
   const { data: { session } } = await supabase.auth.getSession();
   if (!session) window.location.href = "./index.html";
@@ -15,4 +17,9 @@ export async function redirectIfLoggedIn() {
   const { data: { session } } = await supabase.auth.getSession();
   if (session) window.location.href = "./home.html";
   return session;
+}
+
+
+export async function logout() {
+  await supabase.auth.signOut();
 }
